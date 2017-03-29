69244a7c044aef58b5b388079089c9cc44ee303f
Dylan Smith <dylantsmith@gmail.com>
2017-03-29 14:36:21 -0500

    Add webpack middleware
    - Add both webpack dev and hot middleware for development
    - Add a development webpack configuration
    - Dynamically determine the webpack bundle name for the development and
    production environments
    - In development, `nodemon` should not watch all of `app`, but just the
    “server” side of things (which is not yet determined)

--- app/express.js
+++ app/express.js
@@ -8,6 +8,8 @@ import { match, RouterContext } from 'react-router';
 import routes from './routes';
 
 const APP_ROOT = path.join(__dirname, '../');
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const logger = require('./lib/logger')();
 
 const app = express();
@@ -15,11 +17,34 @@ const app = express();
 // use handlebars as the html engine renderer
 app.engine('html', consolidate.handlebars);
 app.set('view engine', 'html');
-app.set('views', path.join(__dirname, 'views'));
+// set this location explicitly for both development and production
+app.set('views', path.join(APP_ROOT, 'app', 'views'));
 
-// serve the static assets (js/css)
-app.use(express.static(path.join(APP_ROOT, 'public')));
+if (IS_DEVELOPMENT) {
+  // only load these dependencies if we are not in production to avoid
+  // requiring them in production mode (when they are only required in dev)
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpackConfig = require('../webpack.config.development');
 
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: 'minimal',
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+if (IS_PRODUCTION) {
+  // serve the static assets (js/css)
+  app.use(express.static(path.join(APP_ROOT, 'public')));
+}
+
+/*
+ * THIS MUST BE THE LAST ROUTE IN THE CONFIG
+ * This renders any other request according to the match rules below
+ */
 app.get('*', (req, res) => {
   logger.log(req.url);
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
@@ -33,14 +58,16 @@ app.get('*', (req, res) => {
       return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
     }
 
-    // this could be a 404 page, but instead, redirect them to the home page
     if (!renderProps) {
       return res.status(404).end();
     }
 
+    const webpackBundleName = IS_DEVELOPMENT ? 'main.js' : 'main.min.js';
+
     // with no errors and no redirects, render the page
     return res.render('index', {
-      content: renderToString(<RouterContext {...renderProps} />),
+      webpackBundleName,
+      htmlContent: renderToString(<RouterContext {...renderProps} />),
     });
   });
 });
