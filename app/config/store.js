339c1fc35310a32934186a2231390cb62f7abf66
Dylan Smith <dylantsmith@gmail.com>
2017-04-03 16:57:10 -0500

    Disable colors in logging on server
    Add options to disable colors for the redux logger when logging on the
    server.

--- app/config/store.js
+++ app/config/store.js
@@ -5,10 +5,17 @@ import { createLogger } from 'redux-logger';
 
 import rootReducer from '../reducers/root.reducer';
 
-const logger = createLogger({
+const loggerOptions = {
   // only log when in development environment
   predicate: () => process.env.NODE_ENV === 'development',
-});
+};
+// special options only used on the server side
+if (typeof window === 'undefined') {
+  Object.assign(loggerOptions, {
+    colors: false,
+  });
+}
+const logger = createLogger(loggerOptions);
 
 function configureStore(initialState, history) {
   const middleware = applyMiddleware(
